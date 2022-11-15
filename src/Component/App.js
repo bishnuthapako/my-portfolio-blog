44c3db7689fb721dafcb2632b1ea26556ef80e75
Bishnu Thapa <bishnu.email.usa@gmail.com>
2022-11-15 10:40:21 -0500

    mui

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import { styled } from '@mui/material';
+
+
 
 function App() {
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#888",
+    margin: 5,
+    color: "red",
+    "&:hover":{
+      backgroundColor: "lightBlue",
+      color: "secondary"
+    }
+
+  })
   return (
-    <div>App</div>
+    <>
+    <div>Welcome to my Blog</div>
+    <Stack direction='row' spacing={2} mt='2'>
+      <Button variant='contained' startIcon={<PsychologyIcon />} color="success">Submit</Button>
+      <Button variant='outlined'>Outlined</Button>
+    </Stack>
+
+    {/* <Button variant='contained'>Singup</Button> */}
+
+    <BlueButton>Signup</BlueButton>
+    
+  
+    </>
+    
+
   )
 }
 
